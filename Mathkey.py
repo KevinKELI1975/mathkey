c42f084e3b5543e266f21b398a2df53515542492
KevinKELI1975 <kevinliang1975@gmail.com>
2025-12-27 23:59:48 +0800

    Update Mathkey.py

--- Mathkey.py
+++ Mathkey.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'hello home'
-    #return render_template('mathkey_homepage.html')
+    #return 'hello home'
+    return render_template('mathkey_homepage.html')
 
 @app.route('/api')
 def api():
-    return 'hello api'
-    #return render_template('mathkey_homepage.html')
+    #return 'hello api'
+    return render_template('mathkey_homepage.html')
 
 @app.route('/Mathkey')
 def Mathkey():
-    return 'hello mathkey'
-    #return render_template('mathkey_homepage.html')
+    #return 'hello mathkey'
+    return render_template('mathkey_homepage.html')
 
 if __name__ == '__main__':
     app.run()
